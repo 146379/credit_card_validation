66722d2e8938c74184c4fe79b9e1b52d893c8d24
Rahul Horé <hore.rahul@gmail.com>
2013-11-22 09:01:50 -0500

    Refactoring Luhn implementation in progress

--- credit_card_validation.rb
+++ credit_card_validation.rb
@@ -1,13 +1,7 @@
 module CreditCardValidation
   def luhn(credit_card_number)
-    digits = credit_card_number.to_s.split('')
-    evens = digits.select.with_index { |d, i| d if i.even? }
-    odds = digits.select.with_index { |d, i| d if i.odd? }
-    evens.map!(&:to_i).map! { |d| 2*d }
-    evens.map! { |d| d > 9 ? 1 + d % 10 : d }
-    evens.reverse!
-    odds.map!(&:to_i)
-    odds.reverse!
-    (evens + odds).reduce(:+)
+    digits = credit_card_number.to_s.split('').map(&:to_i)
+    evens, odds = *digits.each_slice(2).to_a.transpose
+    (evens.map { |d| 2*d > 9 ? 1 + 2*d % 10 : 2*d } + odds).reduce(:+)
   end
 end
