48672a2a4e5b7300a673c954244af1d5b4f3aa5c
Rahul Horé <hore.rahul@gmail.com>
2013-11-23 21:38:32 -0500

    Updates CCV module to validate even digit CC#'s via Luhn

--- credit_card_validation.rb
+++ credit_card_validation.rb
@@ -1,7 +1,21 @@
 module CreditCardValidation
-  def luhn(credit_card_number)
+  
+  # TODO; implement logic to handle odd length CC#'s'
+  def self.luhn(credit_card_number)
     digits = credit_card_number.to_s.split('').map(&:to_i)
     evens, odds = *digits.each_slice(2).to_a.transpose
     (evens.map { |d| 2*d > 9 ? 1 + 2*d % 10 : 2*d } + odds).reduce(:+)
   end
+
+  def self.valid_credit_card?(cc_no)
+  # Determines whether a CC# is valid via Luhn algorithm
+  luhn(cc_no) % 10 == 0
+  end
+
+
 end
+
+
+# cursory testing
+p CreditCardValidation.luhn(5730165506115283)
+p CreditCardValidation.valid_credit_card?(5730165506115283)
