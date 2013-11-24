727e20b3847b6d7b14f226ba84a92d938634f25d
Rahul Horé <hore.rahul@gmail.com>
2013-11-23 21:50:06 -0500

    Adds random 16-digit valid CC# generator

--- credit_card_validation.rb
+++ credit_card_validation.rb
@@ -1,5 +1,5 @@
 module CreditCardValidation
-  
+
   # TODO; implement logic to handle odd length CC#'s'
   def self.luhn(credit_card_number)
     digits = credit_card_number.to_s.split('').map(&:to_i)
@@ -12,10 +12,28 @@ module CreditCardValidation
   luhn(cc_no) % 10 == 0
   end
 
-
+  def self.generate_valid_credit_card_number
+    evens, odds = [rand(9)], []
+    7.times do
+      evens << rand(9)
+      odds << rand(9)
+    end
+    double_evens = evens.map do |digit|
+      (2 * digit).to_s.split('').map(&:to_i).reduce(:+)
+    end
+    checksum = 10 - (double_evens.reduce(:+) + odds.reduce(:+)) % 10
+    evens.zip(odds).flatten.join + checksum.to_s
+  end
 end
 
 
 # cursory testing
-p CreditCardValidation.luhn(5730165506115283)
-p CreditCardValidation.valid_credit_card?(5730165506115283)
+cc = 7821535722532865
+p CreditCardValidation.luhn(cc)
+p CreditCardValidation.valid_credit_card?(cc)
+
+cc2 = CreditCardValidation.generate_valid_credit_card_number
+p cc2
+p CreditCardValidation.luhn(cc2)
+p CreditCardValidation.valid_credit_card?(cc2)
+
